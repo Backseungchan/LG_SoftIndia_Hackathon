29fb9f50209693002daa0a35aab45ff2e8b173a8
KingDonggyu <dgkim69166916@gmail.com>
2021-07-19 19:39:34 +0900

    Create show-description button

--- digital_signage/src/components/DeleteItem.js
+++ digital_signage/src/components/DeleteItem.js
@@ -1,22 +1,31 @@
+import styled from "styled-components";
 import Alert from "@enact/sandstone/Alert";
 import { AlertImage } from "@enact/sandstone/Alert";
 import Button from "@enact/sandstone/Button";
-import BodyText from "@enact/moonstone/BodyText";
-import Scroller from "@enact/sandstone/Scroller";
+import Popup from "@enact/moonstone/Popup";
 
 const DeleteItem = ({ item, handleClickItem, handleDeleteItem }) => {
   const handleClickDelete = () => {
     handleDeleteItem(item._id);
     handleClickItem(false);
   };
+
+  const ItemLayout = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 50px;
+  `;
+
   return (
     <Alert open="True" title="Do you want to delete?">
       <image>
-        <AlertImage src={item.image} type="thumbnail" />
-        <h2 style={{ textAlign: "center" }}>{item.title}</h2>
-        <Scroller>
-          <pre>{item.description}</pre>
-        </Scroller>
+        <ItemLayout>
+          <AlertImage src={item.image} type="thumbnail" />
+          <h2 style={{ textAlign: "center" }}>{item.title}</h2>
+          <Button>Show description</Button>
+        </ItemLayout>
       </image>
       <div style={{ textAlign: "center" }}>
         <Button onClick={handleClickDelete}>Delete</Button>
@@ -26,6 +35,9 @@ const DeleteItem = ({ item, handleClickItem, handleDeleteItem }) => {
           style={{ backgroundColor: "indianred", color: "white" }}
         />
       </div>
+      <Popup open={false}>
+        <pre>{item.description}</pre>
+      </Popup>
     </Alert>
   );
 };
