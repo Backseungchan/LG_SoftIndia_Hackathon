ac95073472046a132a338e7687e99556acc614c4
Seungchan Back <31721097+Backseungchan@users.noreply.github.com>
2021-07-19 15:04:01 +0900

    Create new carousel

--- digital_signage/src/views/ShowPage.js
+++ digital_signage/src/views/ShowPage.js
@@ -1,29 +1,12 @@
-import React, { useState } from "react";
-import InfiniteCarousel from "react-leaf-carousel";
+import React from "react";
 import MainHeaderBar from "../components/common/MainHeaderBar";
+import Carousel from "../components/Carousel";
 
 const ShowPage = ({ items, handleShow }) => {
   return (
     <div>
       <MainHeaderBar handleShow={handleShow} />
-      <InfiniteCarousel
-        dots={true}
-        autoCycle={true}
-        showSides={false}
-        sideSize={0.1}
-        slidesToScroll={1}
-        slidesToShow={1}
-        scrollOnDevice={true}
-      >
-        {items.map((item) => (
-          <div style={{ textAlign: "center" }}>
-            <img
-              src={item.image}
-              style={{ width: "auto", height: "95vh" }}
-            ></img>
-          </div>
-        ))}
-      </InfiniteCarousel>
+      <Carousel items={items} />
     </div>
   );
 };
