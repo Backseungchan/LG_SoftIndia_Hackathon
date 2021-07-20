2d76852d8d32db6ff1b06d472b5e157529d19058
woong-jae <jwchung0828@gmail.com>
2021-07-20 20:53:22 +0900

    Apply Spinner on async call

--- digital_signage/src/App/App.js
+++ digital_signage/src/App/App.js
@@ -3,39 +3,50 @@ import ThemeDecorator from "@enact/sandstone/ThemeDecorator";
 import MoonstoneDecorator from "@enact/moonstone/MoonstoneDecorator";
 import ShowPage from "../views/ShowPage";
 import EditPage from "../views/EditPage";
+import Spinner from '@enact/sandstone/Spinner';
+import styled from "styled-components";
+
 import "./App.css";
 import * as API from "../api/index.js";
+import Alert from "@enact/sandstone/Alert";
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [Showing, setShowing] = useState(true);
+  const [pending, setPending] = useState(false);
   
   useEffect(() => {
-    // API.putKind("com.app.digital-signage", function () {
-    //   console.log("Success putKind");
-    //   API.find("com.app.digital-signage", function (res) {
-    //     setItems(res.results);
-    //     console.log("Success find");
-    //   }, function () {
-    //     console.log("Fail find");
-    //   });
-    // }, function () {
-    //   console.log("Fail putKind");
-    // });
     fetchData();
   }, []);
 
   async function fetchData() {
+    setPending(true);
     const { data } = await API.getData();
+    setPending(false);
     setItems(data);
   }
 
   const handleShow = (where) => {
     setShowing(where);
   };
+
+  const ItemLayout = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 50px;
+  `;
   
   return (
     <div>
+      {pending && 
+        <Alert open="True">
+          <ItemLayout>
+            <Spinner blockClickOn="container">Loading...</Spinner>
+          </ItemLayout>
+        </Alert>
+      }
       {Showing ? (
         <ShowPage items={items} handleShow={handleShow} />
       ) : (
