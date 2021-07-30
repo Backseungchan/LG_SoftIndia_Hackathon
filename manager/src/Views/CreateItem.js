24ab1372824fd9dd856669768326cb6cd7bc4815
KingDonggyu <dgkim69166916@gmail.com>
2021-07-30 17:59:29 +0900

    Release code comment

--- manager/src/Views/CreateItem.js
+++ manager/src/Views/CreateItem.js
@@ -50,8 +50,8 @@ const CreateItem = ({ handleItemList, setPending, items, setItems }) => {
     setPending(true);
     const bodyFormData = new FormData();
     bodyFormData.append("img_url", input.imgBase64);
-    // const res = await API.getImageOCR(bodyFormData);
-    // setInput({ ...input, description: res.data.content });
+    const res = await API.getImageOCR(bodyFormData);
+    setInput({ ...input, description: res.data.content });
     setPending(false);
     setIsSecond(!isSecond);
   };
